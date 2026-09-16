b56cbaedafa38a721b961b63cc9f5c4c838823ce
saifullah <saifullahahad0@gmail.com>
2026-09-16 19:32:54 +0530

    get index

--- getElement.java
+++ getElement.java
@@ -1,5 +1,6 @@
 
 
+
 class Node {
 int vel;
 Node next;
@@ -8,7 +9,7 @@ Node(int vel){
 }
     
 }
-public class linklist {
+public class getElement{
 
     public static void print(Node head){
      Node temp = head;
@@ -18,17 +19,25 @@ public class linklist {
      }
     }
 
+    public static int get(Node head, int idx){
+       Node temp = head;
+       for(int i = 0; i<=idx; i++){
+        head = temp.next;
+       }
+       return head.vel;
+    }
+
     public static void main(String[] args) {
-       Node a = new Node(10);
-         Node b = new Node(10);
-          Node c = new Node(10);
-           Node d = new Node(10);
-        Node e = new Node(10);
+       Node a = new Node(1);
+         Node b = new Node(2);
+          Node c = new Node(3);
+           Node d = new Node(4);
+        Node e = new Node(5);
 
             a.next = b;
             b.next = c;
             c.next = d;
             d.next = e; 
-            print(a);
+           System.out.print(get(a, 2)); 
     }
 }
