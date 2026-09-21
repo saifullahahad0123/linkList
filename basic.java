f4af6a376f6187d48770b7db9304fbdb6f488e7c
saifullah <saifullahahad0@gmail.com>
2026-09-21 19:32:58 +0530

    print value in doubble linked list

--- basic.java
+++ basic.java
@@ -1,45 +1,85 @@
+class DNode {
 
- class DNode {
- int val;
- DNode next;
- DNode prve;
- DNode (int val) {
-    this.val = val;
-}};
+    int val;
+    DNode next;
+    DNode prve;
 
-class Add{
-    DNode head;
-    DNode tail;
-    int  count;
-    void  addAtHead(int val){
-    DNode temp = new DNode(val) ;
-    if (head == null) head = tail = temp;  
-    else {
-        temp.next = head;
-        head.prve = temp;
-        temp = head;
-    }
-    }
-
-     void  addAttail(int val){
-    DNode temp = new DNode(val) ;
-    if (head == null) head = tail = temp;  
-    else {
-        temp.next = temp;
-        head.prve = tail;
-        temp = head;
-    }
+    DNode(int val) {
+        this.val = val;
     }
 }
 
+class Add {
+
+    DNode head;
+    DNode tail;
+    int count;
+
+    // Add at head
+    void addAtHead(int val) {
+
+        DNode temp = new DNode(val);
+
+        if (head == null) {
+            head = tail = temp;
+        } 
+        else {
+            temp.next = head;
+            head.prve = temp;
+            head = temp;
+        }
+
+        count++;
+    }
+
+    // Add at tail
+    void addAtTail(int val) {
+
+        DNode temp = new DNode(val);
+
+        if (head == null) {
+            head = tail = temp;
+        } 
+        else {
+            tail.next = temp;
+            temp.prve = tail;
+            tail = temp;
+        }
+
+        count++;
+    }
+
+    // Print from head
+    void print() {
+
+        DNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+}
 
 public class basic {
 
-public static void main(String[] args) {
-    
-Add a = new Add();
-a.addAtHead(55);
+    public static void main(String[] args) {
 
-}
-    
+        Add a = new Add();
+
+        a.addAtHead(55);
+        a.addAtHead(2);
+        a.addAtHead(5);
+        a.addAtHead(45);
+        a.addAtHead(65);
+
+        a.print();
+
+        a.addAtTail(100);
+        a.addAtTail(200);
+
+        a.print();
+    }
 }
